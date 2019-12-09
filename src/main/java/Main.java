e167e745d57b8b7862906a0c48fb3f09d57a1369
Haruto Shiratori <e195701@ie.u-ryukyu.ac.jp>
2019-12-09 15:48:56 +0900

    Add Exception handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException nuru){
+            System.out.println("\u001b[00;31m" + "Exception in thread \"main\" java.lang.虚無虚無プリン" + "\u001b[00m");
+            System.out.println(nuru.getMessage());
+        }
 
-        String str = null;
-        System.out.println(str.length());
     }
 }
