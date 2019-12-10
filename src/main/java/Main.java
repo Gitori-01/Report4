01385544a565afd7b0edf85c9fc95acaad3cc075
Haruto Shiratori <e195701@ie.u-ryukyu.ac.jp>
2019-12-10 10:06:47 +0900

    Delete Step1&2 Add Step3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException nuru){
-            System.out.println("\u001b[00;31m" + "Exception in thread \"main\" java.lang.虚無虚無プリン" + "\u001b[00m");
-            System.out.println(nuru.getMessage());
-        }
-
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
